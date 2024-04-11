e225732790aaca62dfef31232796e1e720057ef2
Ferrostol <73596526+Ferrostol@users.noreply.github.com>
2024-04-11 22:16:04 +0700

    Изменение способа удаления сессий

--- src/server.py
+++ src/server.py
@@ -1,10 +1,37 @@
 import re
 from config import log_file, output_file
 import subprocess
+import os
+
 
 ip_pattern = r'<addr\s+(\d+\.\d+\.\d+\.\d+)>'
+ip_ps_pattern = r'\d+\.\d+\.\d+\.\d+\:(\d+\.\d+\.\d+\.\d+)'
 
-def get_ip_user(username):
+
+def get_all_processes():
+    try:
+        # Выполнение команды ps с grep
+        result = subprocess.run('/usr/bin/ps aux', shell=True, capture_output=True, text=True)
+        # Поиск строк, содержащих xl2tpd и IP-адрес
+        processes = []
+        for line in result.stdout.split('\n'):
+            if 'pppd plugin' in line:
+                ip_ps = re.search(ip_ps_pattern, line).group(1)
+                processes.append(
+                    (
+                        line.split()[1],
+                        ip_ps,
+                        get_last_name_for_ip(ip_ps)
+                    )
+                )
+        if not len(processes):
+            delete_file_logs()
+        return processes
+    except Exception as e:
+        return str(e)
+    
+
+def get_last_name_for_ip(ip_address):
     try:
         # Читаем содержимое файла
         with open(log_file, 'r') as file:
@@ -12,43 +39,41 @@ def get_ip_user(username):
         
         # Ищем последнее вхождение подстроки
         last_match_index = -1
-        for i in range(len(lines)):
-            if username in lines[i]:
+        ln_line = len(lines)
+        for i in range(ln_line - 1, -1, -1):
+            if 'ConfNak' in lines[i] and ip_address in lines[i]:
                 last_match_index = i
+                break
         
         if last_match_index == -1:
             return None
         
         # Ищем строку с "ConfNak" после последнего вхождения подстроки
-        for i in range(last_match_index, len(lines)):
-            if "ConfNak" in lines[i]:
-                ip_address_match = re.search(ip_pattern, lines[i])
-                return ip_address_match.group(1)
+        for i in range(last_match_index - 1, -1, -1):
+            if 'CHAP Response' in lines[i]:
+                return re.search(r'name\s+=\s+\"(\w+)\"', lines[i]).group(1)
         return None
 
     except FileNotFoundError:
         return None
     
-def get_processes(ip_address):
-    try:
-        # Выполнение команды ps с grep
-        result = subprocess.run('/usr/bin/ps aux', shell=True, capture_output=True, text=True)
-        # Поиск строк, содержащих xl2tpd и IP-адрес
-        processes = [line for line in result.stdout.split('\n') if 'xl2tpd' in line and ip_address in line]
-        return processes
-    except Exception as e:
-        return str(e)
 
 def delete_session(username):
     try:
-        ip_address = get_ip_user(username)
-        processes = get_processes(ip_address)
+        # Выполнение команды ps с grep
+        result = get_all_processes()
+        # Поиск строк, содержащих xl2tpd и IP-адрес
+        processes = [info for info in result if info[2] == username]
         for proc in processes:
-            pid = proc.split()[1]
-            subprocess.run(f'/usr/bin/kill {pid}', shell=True, capture_output=True, text=True)
+            subprocess.run(f'/usr/bin/kill {proc[0]}', shell=True, capture_output=True, text=True)
         return True
     except Exception:
         return False
+
+
+def delete_file_logs():
+    if os.path.exists(log_file):
+        os.remove(log_file)
 
 
 def write_users_to_file(users):
